f9c6361078acdf1ef88c30649299fd595ea83d52
jcozar87 <javier.cozar@uclm.es>
2016-12-19 23:32:08 +0100

    Initial - API info in comments

--- app/views.py
+++ app/views.py
@@ -12,8 +12,36 @@ def init():
 
 @app.route('/visionapi', methods=['GET','POST'])
 def visionapi():
+    '''
+    json request:
+    {'requests':[{'image':{'content':data stream},
+                  'features':[{'type':'LABEL_DETECTION','maxResults': N},
+                                {'type':'TEXT_DETECTION'}]
+                 }]}
+    '''
+    '''
+    post request:
+        requests.post(vision_api_url+'?key='+key, data = json.dumps(json request))
+    '''
+
     return render_template('visionapi.html', request = 'visionapi')
 
 @app.route('/speechapi', methods=['GET','POST'])
 def speechapi():
+    '''
+    json request:
+    {'config': {'encoding': encoding, 'sampleRate':sampleRate, 'languageCode': LANG},
+                'audio': {'content': data stream}}
+    '''
+    '''
+    post request:
+        requests.post(speech_api_url+'?key='+key, data = json.dumps(json request))
+    '''
     return render_template('speechapi.html', request = 'speechapi')
+
+'''
+-translate API-
+post request:
+    requests.post(translate_api_url, data = dict(key = key, source = inputLang,
+                                        target = outputLang, q = list of texts))
+'''
